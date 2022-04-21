4bf251a668065af6c180397d563ca46c7a05ed02
gzamaury <gzamaury@hotmail.com>
2022-04-21 16:16:44 -0500

    Renaming folder

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import {
 } from 'react-router-dom'
 
 import { Navbar } from './app/Navbar'
-import { AddPostForm } from './features/post/AddPostForm'
-import { EditPostForm } from './features/post/EditPostForm'
-import { PostsList } from './features/post/PostsList'
-import { SinglePostPage } from './features/post/SinglePostPage'
+import { AddPostForm } from './features/posts/AddPostForm'
+import { EditPostForm } from './features/posts/EditPostForm'
+import { PostsList } from './features/posts/PostsList'
+import { SinglePostPage } from './features/posts/SinglePostPage'
 
 function App() {
   return (

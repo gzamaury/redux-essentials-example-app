3e07f906c8fea8f35476531bd5914d6559c930e1
gzamaury <gzamaury@hotmail.com>
2022-04-21 13:42:09 -0500

    More Post Features - Storing Dates for Posts

--- src/features/post/PostsList.js
+++ src/features/post/PostsList.js
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
 import { selectPosts } from './postsSlice'
+import { TimeAgo } from './TimeAgo'
 
 export const PostsList = () => {
   const posts = useSelector(selectPosts)
@@ -10,6 +11,7 @@ export const PostsList = () => {
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
       <PostAuthor userId={post.user} />
+      <TimeAgo timestamp={post.date} />
       <p className="post-content">{post.content.substring(0, 100)}</p>
       <Link to={`/posts/${post.id}`} className="button muted-button">
         View Post

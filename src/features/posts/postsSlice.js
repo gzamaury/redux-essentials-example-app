cbd33e8e7249e8cc0157672942da64849c38ac35
gzamaury <gzamaury@hotmail.com>
2022-04-29 17:10:14 -0500

    Adding New Posts - Sending Data with Thunks

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -8,6 +8,17 @@ const initialState = {
   error: null,
 }
 
+export const addNewPost = createAsyncThunk(
+  'posts/addNewPost',
+  // The payload creator receives the partial `{title, content, user}` object
+  async (initialPost) => {
+    // We send the initial data to the fake API server
+    const response = await client.post('/fakeApi/posts', initialPost)
+    // The response includes the complete post object, including unique ID
+    return response.data
+  }
+)
+
 export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
   const response = await client.get('/fakeApi/posts')
   return response.data
@@ -85,6 +96,10 @@ const postsSlice = createSlice({
       .addCase(fetchPosts.rejected, (state, action) => {
         state.status = 'failed'
         state.error = action.error.message
+      })
+      .addCase(addNewPost.fulfilled, (state, action) => {
+        // We can directly add the new post object to our posts array
+        state.posts.push(action.payload)
       })
   },
 })

739eab99151c30710c73cf23a9b2f2b682d3f085
gzamaury <gzamaury@hotmail.com>
2022-04-29 17:10:36 -0500

    Checking Thunk Results in Components

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { selectUsers } from '../users/usersSlice'
-import { postAdded } from './postsSlice'
+import { addNewPost } from './postsSlice'
 
 export const AddPostForm = () => {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
   const [userId, setUserId] = useState('')
+  const [addRequestStatus, setAddRequestStatus] = useState('idle')
 
   const dispatch = useDispatch()
 
@@ -16,22 +17,38 @@ export const AddPostForm = () => {
   const onContentChanged = (e) => setContent(e.target.value)
   const onAuthorChanged = (e) => setUserId(e.target.value)
 
-  const onSavePostClicked = () => {
-    if (title && content) {
-      dispatch(
-        /* Now our component doesn't have to worry about what the payload object
-          looks like - the action creator will take care of putting it together
-          the right way. So, we can update the component so that it passes in
-          title and content as arguments when it dispatches postAdded */
-        postAdded(title, content, userId)
-      )
+  const canSave =
+    [title, content, userId].every(Boolean) && addRequestStatus === 'idle'
 
-      setTitle('')
-      setContent('')
+  const onSavePostClicked = async () => {
+    if (canSave) {
+      /* When we call dispatch(addNewPost()), the async thunk returns a Promise 
+        from dispatch. We can await that promise here to know when the thunk has 
+        finished its request. But, we don't yet know if that request succeeded or 
+        failed. createAsyncThunk() handles any errors internally, so that we don't 
+        see any messages about "rejected Promises" in our logs. It then returns the 
+        final action it dispatched: either the fulfilled action if it succeeded, or 
+        the rejected action if it failed.
+
+        However, it's common to want to write logic that looks at the success or 
+        failure of the actual request that was made. Redux Toolkit adds a .unwrap() 
+        function to the returned Promise, which will return a new Promise that either 
+        has the actual action.payload value from a 'fulfilled' action, or throws an error 
+        if it's the 'rejected' action. This lets us handle success and failure in the 
+        component using normal try/catch logic. */
+      try {
+        setAddRequestStatus('pending')
+        await dispatch(addNewPost({ title, content, user: userId })).unwrap()
+        setTitle('')
+        setContent('')
+        setUserId('')
+      } catch (err) {
+        console.error('Failed to save the post: ', err)
+      } finally {
+        setAddRequestStatus('idle')
+      }
     }
   }
-
-  const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
 
   const usersOptions = users.map((user) => (
     <option key={user.id} value={user.id}>

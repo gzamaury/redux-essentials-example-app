5bc7e9ee670448ff982751ec47f2ef792e2a29e7
gzamaury <gzamaury@hotmail.com>
2022-04-30 16:57:57 -0500

    Improving Render Performance - Investigating Render Behavior

--- src/features/users/UserPage.js
+++ src/features/users/UserPage.js
@@ -8,6 +8,21 @@ export const UserPage = ({ match }) => {
 
   const user = useSelector(selectUserById(userId))
 
+  /* We can use the React DevTools Profiler to view some graphs of what components 
+    re-render when state is updated. Try clicking over to the <UserPage> for a 
+    single user. Open up your browser's DevTools, and in the React "Profiler" tab, 
+    click the circle "Record" button in the upper-left. Then, click the "Refresh 
+    Notifications" button in our app, and stop the recording in the React DevTools 
+    Profiler. You should see that the <Navbar> re-rendered, which makes sense because 
+    it had to show the updated "unread notifications" badge in the tab. But, why did 
+    our <UserPage> re-render?
+
+    We know that useSelector will re-run every time an action is dispatched, and that 
+    it forces the component to re-render if we return a new reference value. We're 
+    calling filter() inside of our useSelector hook, so that we only return the list 
+    of posts that belong to this user. Unfortunately, this means that useSelector 
+    always returns a new array reference, and so our component will re-render after 
+    every action even if the posts data hasn't changed!. */
   const postsForUser = useSelector((state) => {
     const allPosts = selectAllPosts(state)
     return allPosts.filter((post) => post.user === userId)

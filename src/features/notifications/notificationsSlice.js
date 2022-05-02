e80cfd6fa5c4f7341480974a8702088bd62e07be
gzamaury <gzamaury@hotmail.com>
2022-05-01 20:48:47 -0500

    Converting the Notifications Slice

--- src/features/notifications/notificationsSlice.js
+++ src/features/notifications/notificationsSlice.js
@@ -1,5 +1,13 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import {
+  createAsyncThunk,
+  createEntityAdapter,
+  createSlice,
+} from '@reduxjs/toolkit'
 import { client } from '../../api/client'
+
+const notificationsAdapter = createEntityAdapter({
+  sortComparer: (a, b) => b.date.localeCompare(a.date),
+})
 
 /* Thunk Arguments - For createAsyncThunk specifically, you can only pass in one argument, 
   and whatever we pass in becomes the first argument of the payload creation callback. The 
@@ -38,10 +46,10 @@ export const fetchNotifications = createAsyncThunk(
 
 const notificationsSlice = createSlice({
   name: 'notifications',
-  initialState: [],
+  initialState: notificationsAdapter.getInitialState(),
   reducers: {
     allNotificationsRead(state, action) {
-      state.forEach((notification) => {
+      Object.values(state.entities).forEach((notification) => {
         notification.read = true
       })
     },
@@ -49,13 +57,11 @@ const notificationsSlice = createSlice({
   extraReducers: {
     // @ts-ignore
     [fetchNotifications.fulfilled]: (state, action) => {
-      state.push(...action.payload)
-      state.forEach((notification) => {
+      notificationsAdapter.upsertMany(state, action.payload)
+      Object.values(state.entities).forEach((notification) => {
         // Any notifications we've read are no longer new
         notification.isNew = !notification.read
       })
-      // Sort with newest first
-      state.sort((a, b) => b.date.localeCompare(a.date))
     },
   },
 })
@@ -64,4 +70,11 @@ export const { allNotificationsRead } = notificationsSlice.actions
 
 export default notificationsSlice.reducer
 
-export const selectAllNotifications = (state) => state.notifications
+const {
+  selectAll: selectAllNotifications,
+  selectById,
+  selectIds: selectNotificationIds,
+} = notificationsAdapter.getSelectors((state) => state.notifications)
+const selectNotificationById = (notificationId) => (state) =>
+  selectById(state, notificationId)
+export { selectAllNotifications, selectNotificationById, selectNotificationIds }
